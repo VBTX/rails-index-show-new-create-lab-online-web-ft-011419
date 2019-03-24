d6c50ad60d88e8221be9a4765800fc6a02eb52a5
Victoria B <viktorija.bourassa@gmail.com>
2019-03-24 12:55:52 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,10 +1,15 @@
 class CouponsController < ApplicationController
+  def index
+    @coupons = Coupon.all
+  end
+
+  def new
+  end
   def create
     coupon = Coupon.new
     coupon.coupon_code = params[:coupon_code]
     coupon.store = params[:store]
     coupon.save
     redirect_to coupons_path(@coupon)
-
   end
 end
